401cc5c6ca5b988441c2c4ba0d680144a82318ac
Mohit kumar paul <mohit1paul@gmail.com>
2025-02-24 14:25:35 +0530

    fisrt programme

---
+++ FirstProgram/test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int age;
+    printf("please enter your age:");
+    scanf("%d", &age);
+    printf("your age is: %d",  age);
+
+    // print the first program 
+    // printf("Hello world\n");
+    // printf("my age is %d\n", 49);
+    // printf("my first programe");
+
+    return 0;
+}
